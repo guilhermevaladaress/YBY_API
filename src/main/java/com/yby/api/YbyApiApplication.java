d9b39bd6d076a837b558f8ab1ee0544a81911f89
Guilherme Valadares <guilhermevaladares007@gmail.com>
2026-05-30 08:54:14 -0300

    first commit

---
+++ src/main/java/com/yby/api/YbyApiApplication.java
@@ -0,0 +1,13 @@
+package com.yby.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YbyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YbyApiApplication.class, args);
+	}
+
+}
